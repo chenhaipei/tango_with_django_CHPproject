dcda8cfc0fd23aadd20fcca58cf92c2d4274e0d2
Haipei Chen <2637279C@student.gla.ac.uk>
2022-02-05 22:09:43 +0800

    The 'Title' column could not be found in the admin interface for the Page model

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
@@ -12,6 +13,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
